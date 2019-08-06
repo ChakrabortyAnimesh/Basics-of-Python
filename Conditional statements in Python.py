2b82bebadf1f5a596cbfad05a828d229895403e3
Animesh Chakraborty <animesh.b.tech12345@gmail.com>
2019-08-06 17:49:05 +0530

    Check whether the number is Odd or even

---
+++ Conditional statements in Python.py
@@ -0,0 +1,9 @@
+#Finding the given no. is odd or even
+
+num=int(input("Enter any integer number ="))
+if (num%2)==0:
+    print("%d is even number." %num)
+else:
+    print("%d is odd number." %num)	
+
+input("Enter any key to exit")	
