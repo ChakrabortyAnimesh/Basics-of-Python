61ab34207044f2a4ba57949c09e9caa369a68caa
Animesh Chakraborty <animesh.b.tech12345@gmail.com>
2019-08-06 17:42:34 +0530

    Input from user and find the sum of the given no.

---
+++ Input From Keyboard and Find sum.py
@@ -0,0 +1,8 @@
+arg1=float(input("Enter first no. ="))
+arg2=float(input("Enter second no. ="))
+def add(a,b):
+    return (a + b)
+	
+	
+print("The sum is =",add(arg1,arg2))
+input("please enter to exit")
